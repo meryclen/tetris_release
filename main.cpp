b7fa74273121b4b5308cf20c6b52d7a39ca29857
merycle <yamaninken3@hotmail.com>
2025-01-27 06:48:32 +0900

    ゲームオーバーしてもスペース押すと続行できるようにした

--- main.cpp
+++ main.cpp
@@ -7,51 +7,71 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR IpCmdLine, int nCmdShow)
 {
 
-	ChangeWindowMode(TRUE);
 
-	SetGraphMode(1280, 720, 32);
-
-	//垂直同期
-	//SetWaitVSyncFlag(FALSE);
-
-	SetCreateSoundDataType(DX_SOUNDDATATYPE_MEMNOPRESS);
-
-	SetAlwaysRunFlag(TRUE);
-
-	if (DxLib_Init() == -1)
+	while (true)
 	{
-		return -1;
-	}
+		bool endFlag = false;
 
-	SetDrawScreen(DX_SCREEN_BACK);
+		ChangeWindowMode(TRUE);
 
-	
-	ScreenFlip();
+		SetGraphMode(1280, 720, 32);
 
-	int MoveFlag = 1;
-	
-	while (!(CheckHitKey(KEY_INPUT_SPACE)))
-	{
-		ClearDrawScreen();
-		DrawFormatString(500, 650, GetColor(200, 0, 0), "Press Space Button\n");
+		//垂直同期
+		//SetWaitVSyncFlag(FALSE);
+
+		SetCreateSoundDataType(DX_SOUNDDATATYPE_MEMNOPRESS);
+
+		SetAlwaysRunFlag(TRUE);
+
+		if (DxLib_Init() == -1)
+		{
+			return -1;
+		}
+
+		SetDrawScreen(DX_SCREEN_BACK);
+
+
 		ScreenFlip();
-	}
-	
-	//GAME* g = new GAME();
-	{
-		GAME gameObject;
-		gameObject.run();
-	}
 
-	while (!(CheckHitKey((KEY_INPUT_RETURN))))
-	{
-		//ClearDrawScreen();
-		DrawFormatString(500, 700, GetColor(200, 0, 0), "エンターキーを押すと終了します\n");
-		ScreenFlip();
-	}
+		int MoveFlag = 1;
 
-	
-	DxLib_End();
+		while (!(CheckHitKey(KEY_INPUT_SPACE)))
+		{
+			ClearDrawScreen();
+			DrawFormatString(500, 650, GetColor(200, 0, 0), "Press Space Button\n");
+			ScreenFlip();
+		}
+
+		//GAME* g = new GAME();
+		{
+			GAME gameObject;
+			gameObject.run();
+		}
+
+		while (!(CheckHitKey(KEY_INPUT_RETURN)))
+		{
+			//ClearDrawScreen();
+			DrawFormatString(500, 700, GetColor(200, 0, 0), "エンターキーを押すと終了します\n");
+			DrawFormatString(500, 730, GetColor(200, 0, 0), "スペースキーを押すと続行します\n");
+			ScreenFlip();
+
+			if (CheckHitKey(KEY_INPUT_RETURN))
+			{
+				endFlag = true;
+				break;
+			}
+
+			if (CheckHitKey(KEY_INPUT_SPACE))
+			{
+				endFlag = false;
+				break;
+			}
+		}
+
+		if(endFlag) break;
+
+	}
+		DxLib_End();
 
 	return _CrtDumpMemoryLeaks();
 }
